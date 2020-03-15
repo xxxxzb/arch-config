06793ee521cdb514dc6b6b6a23f3e3835c00940e
xxxxzb <benzjf@live.com>
2020-03-15 21:51:07 +0800

    arch config

---
+++ fish/config.fish
@@ -0,0 +1,12 @@
+# Start X at login
+if status is-login
+    if test -z "$DISPLAY" -a $XDG_VTNR = 1
+        exec startx -- -keeptty
+    end
+end
+
+export PATH="$HOME/.cargo/bin:$PATH"
+
+export PATH="$HOME/App/qemu-4.1.1/riscv32-softmmu:$HOME/App/qemu-4.1.1/riscv64-softmmu:$PATH"
+
+ set VISUAL nvim; export VISUAL EDITOR=nvim; export EDITOR
---
+++ fish/functions/l.fish
@@ -0,0 +1,4 @@
+# Defined in - @ line 1
+function l --description 'alias l lsd -la'
+	lsd -la $argv;
+end
