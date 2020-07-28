f369008a78f2f56f0b38b6dee0d3608fb650f90e
xxxxzb <benzjf@live.com>
2020-07-28 19:58:44 +0800

    ..

--- fish/config.fish
+++ fish/config.fish
@@ -6,6 +6,7 @@ if status is-login
 end
 
 export PATH="$HOME/.cargo/bin:$PATH"
+export OPENSSL_DIR=/usr
 
 #export PATH="$HOME/App/qemu-4.1.1/riscv32-softmmu:$HOME/App/qemu-4.1.1/riscv64-softmmu:$PATH"
 
